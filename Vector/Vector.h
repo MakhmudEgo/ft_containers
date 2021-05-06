408d0d83bb51b078a0e89749b3a1af40a3f422c8
MakhmudEgo <makhmudego@yandex.ru>
2021-05-06 17:07:23 +0300

    decl Vector

---
+++ Vector/Vector.h
@@ -0,0 +1,5 @@
+//
+// Created by Mahmud Jego on 5/6/21.
+//
+
+#pragma once
---
+++ Vector/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mahmud Jego on 5/6/21.
+//
+
+#include "Vector.h"
+#include "overloadNewAndDelete.h"
+
+int main() {
+
+}
