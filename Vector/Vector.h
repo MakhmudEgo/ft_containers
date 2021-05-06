bb0cdf634b50dfd09df762898b23138ed92b312a
MakhmudEgo <makhmudego@yandex.ru>
2021-05-06 19:16:25 +0300

    decl template class Vector; add constructors, size, resize, capacity, reserve, overload []

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -3,3 +3,105 @@
 //
 
 #pragma once
+
+#include <memory>
+
+namespace ft {
+	template<
+			class T,
+			class Allocator = std::allocator<T>
+	>
+	class vector {
+	public:
+		explicit vector( const Allocator &alloc = Allocator() )
+				: _pVector( 0x0 ), _alloc( alloc ), _sz( 0 ), _cp( 0 ) {}
+
+		explicit vector( size_t count,
+				  const T &value = T(),
+				  const Allocator &alloc = Allocator() )
+				  : _alloc( alloc ), _sz( 0 ), _cp(0) {
+			reserve(count);
+			_sz = count;
+			for (size_t i = 0; i < count; ++i) {
+				_alloc.construct(_pVector + i, value);
+			}
+		}
+
+/*		template<class InputIt>
+		vector(InputIt first, InputIt last,
+		 const Allocator &alloc = Allocator()) {
+
+		}*/
+
+		vector(const vector &other) {
+			if(_pVector != &other) {
+				//
+			}
+		}
+
+		size_t size() const {
+			return _sz;
+		}
+
+		size_t capacity() const {
+			return _cp;
+		}
+
+		void resize( size_t count, T value = T() ) {
+			if (count > _cp) {
+				size_t tmp = _cp;
+				while (tmp < count) {
+					tmp *= 2;
+				}
+				reserve(tmp);
+				for (size_t i = _sz; i < count; ++i) {
+					_alloc.construct(_pVector + i, value);
+				}
+			} else {
+				if (count < _sz) {
+					for (size_t i = count - 1; i < _sz; ++i) {
+						_alloc.destroy(_pVector + i);
+					}
+				} else if (count != _sz) {
+					for (size_t i = _sz; i < count; ++i) {
+						_alloc.construct(_pVector + i, value);
+					}
+				}
+			}
+			_sz = count;
+		}
+
+		void reserve( size_t new_cap ) {
+			if (new_cap <= _cp) {
+				return;
+			}
+			T* newVector = _alloc.allocate(new_cap);
+//			std::uninitialized_copy(_pVector, _pVector + _sz, newVector); // TODO:: when be iter
+			{
+				for (size_t i = 0; i < _sz; ++i) {
+					_alloc.construct(newVector + i, _pVector[i]);
+				}
+			}
+			for (size_t i = 0; i < _sz; ++i) {
+				_alloc.destroy(_pVector + i);
+			}
+			_alloc.deallocate(_pVector, _cp);
+			_pVector = newVector;
+			_cp = new_cap;
+		}
+
+		T& operator[](size_t n) {
+			return _pVector[n];
+		}
+
+		const T& operator[](size_t n) const {
+			return _pVector[n];
+		}
+
+	private:
+		T *_pVector;
+		Allocator _alloc;
+		size_t _sz;
+		size_t _cp;
+	};
+}
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -2,9 +2,28 @@
 // Created by Mahmud Jego on 5/6/21.
 //
 
+#include <iostream>
 #include "Vector.h"
+#include <vector>
 #include "overloadNewAndDelete.h"
 
 int main() {
+	ft::vector<int> hello(5, 1234);
+//	hello.clear();
+	hello.resize(10, 1);
+	hello.resize(3);
+	hello.resize(10, 12);
+	hello.reserve(1);
+//	hello.reserve(15);
+	std::cout << hello.size() << std::endl;
+	std::cout << hello.capacity() << std::endl;
+	for (size_t i = 0; i < hello.size(); ++i) {
+		std::cout << hello[i] << ' ';
+	}
+//	std::allocator<int> al;
+//	al.()
+
+
+	return 0;
 
 }
