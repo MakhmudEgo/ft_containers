e9c2a4c0f9319a386b30a038d95faa1152984e67
MakhmudEgo <makhmudego@yandex.ru>
2021-05-08 16:26:22 +0300

    refact main

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -2,57 +2,18 @@
 // Created by Mahmud Jego on 5/6/21.
 //
 
-#include <iostream>
-#include "Vector.h"
-#include <algorithm>
-#include <numeric>
-#include <vector>
 //#include "overloadNewAndDelete.h"
+
 #include "unit_tests_vector.hpp"
+#include "unit_test_iterator.hpp"
 
 int main() {
-/*	ft::vector<int> hello(5, 1234);
-//	hello.clear();
-
-
-	hello.resize(10, 1);
-	hello.resize(3);
-	hello.resize(10, 12);
-	hello.reserve(1);
-//	hello.reserve(15);
-	std::cout << hello.size() << std::endl;
-	std::cout << hello.capacity() << std::endl;
-	for (size_t i = 0; i < hello.size(); ++i) {
-		std::cout << hello[i] << ' ';
-	}
-
-	ft::vector<int>::const_iterator const_it(hello.begin());
-	ft::vector<int>::iterator it(hello.begin());
-
-
-	std::cout << std::endl << *const_it << " this is iter =========" << std::endl;
-	std::cout << std::endl << *it << " this is iter ===========" << std::endl;
-
-	*it++;
-	*it;
-	*it = 2;
-
-	std::cout << std::endl << *const_it << " this is iter" << std::endl;
-	std::cout << std::endl << *it << " this is iter" << std::endl;
-//	std::allocator<int> al;
-//	al.()
-
-//	auto it = hello.begin();
-	ft::vector<bool> gg;
-	gg.hello();*/
-
 	{
 		TestRunner tr;
-
-		tr.RunTest(unit_tests_vector, "df");
+//		tr.RunTest(unit_tests_vector, "base test");
+//		tr.RunTest(unit_size, "Size");
+		tr.RunTest(unit_test_iterator, "iterator");
 	}
-
-
 	return 0;
 
 }
---
+++ colors.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Mahmud Jego on 5/8/21.
+//
+
+#pragma once
+
+#define _FT_ "\x1b[1;33m"
+#define _STD_ "\x1b[1;34m"
+#define RESET_WHOSE "\x1b[0m"
