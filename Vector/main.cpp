76ad61c5eda7364a37a9b01eae28b6a28aa25687
MakhmudEgo <makhmudego@yandex.ru>
2021-05-11 22:52:24 +0300

    upd main

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -8,7 +8,8 @@
 //#include "unit_tests_vector.hpp"
 //#include "unit_test_iterator.hpp"
 //#include "unit_test_size.hpp"
-#include "unit_test_assign.hpp"
+//#include "unit_test_assign.hpp"
+#include "unit_test_erase.hpp"
 #include "Vector.h"
 #include "test_runner.h"
 #include <numeric>
@@ -20,17 +21,10 @@ int main() {
 //		tr.RunTest(unit_tests_vector, "base test");
 //		tr.RunTest(unit_size, "Size");
 //		tr.RunTest(unit_test_iterator, "iterator");
-//		tr.RunTest(unit_test_assign, "assign");/
+//		tr.RunTest(unit_test_assign, "assign");
+		tr.RunTest(unit_test_erase, "erase");
 	}
 
 
-	ft::vector<int> v(15,5);
-
-	ft::vector<int> v1;
-	ft::vector<int>::iterator it1 = v.begin();
-	std::cout << *it1 << std::endl;
-	v1.assign(v.begin(), v.end());
-	std::cout << v1.capacity() << std::endl;
-	PrintV(v1, std::cout);
 	return 0;
 }
