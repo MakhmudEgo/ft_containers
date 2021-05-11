780634dee78edfb28b41b8e87f363e0ab3665043
MakhmudEgo <makhmudego@yandex.ru>
2021-05-11 22:51:50 +0300

    upd vector: add member fucs

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -7,6 +7,7 @@
 #include <memory>
 #include "Iterator.h"
 #include <iostream>
+#include <stdexcept>
 
 
 template<bool B, class T = void>
@@ -37,22 +38,17 @@ namespace ft {
 		typedef RandomAccess<T>						iterator;
 		typedef ConstRandomAccess<T>				const_iterator;
 
-		// ***** Member types ***** //
+		// *************** Member types *************** //
 
-		// ***** iter ***** //
+		// *************** iter *************** //
 
-		iterator begin() { return _pVector; }
-		iterator end() { return _pVector + _sz; }
+		iterator begin() { return _v; }
+		iterator end() { return _v + _sz; }
 
-		// ***** iter ***** //
-
-
-
-
-
+		// *************** iter *************** //
 
 		explicit vector( const Allocator &alloc = Allocator() )
-				: _pVector( 0x0 ), _alloc( alloc ), _sz( 0 ), _cp( 0 ) {}
+				: _v( 0x0 ), _alloc( alloc ), _sz(0 ), _cp(0 ) {}
 
 		explicit vector( size_t count,
 				  const T &value = T(),
@@ -61,21 +57,21 @@ namespace ft {
 			default_init(count, value);
 		}
 
-/*		template<class InputIt>
+		template< typename InputIt >
 		vector(InputIt first, InputIt last,
-		 const Allocator &alloc = Allocator()) {
-
-		}*/
+		 const Allocator &alloc = Allocator(),
+		 typename enable_if<!std::is_integral<InputIt>::value>::type * = 0)
+		 : _v(0x0 ), _alloc(alloc), _sz(0 ), _cp(0 ) {
+			assign(first, last);
+		}
 
 		vector(const vector &other) {
-			if(_pVector != &other) {
+			if(_v != &other) {
 				//
 			}
 		}
 
-		void assign( size_type count, const T& value ) {
-			default_init(count, value);
-		}
+		void assign( size_type count, const T& value ) { default_init( count, value ); }
 
 		template< typename InputIt >
 		typename enable_if<!std::is_integral<InputIt>::value, void>::type
@@ -84,11 +80,37 @@ namespace ft {
 			_sz = last - first;
 
 			for (size_t i = 0; first != last; ++first, i++) {
-				_alloc.construct(this->_pVector + i, *first);
+				_alloc.construct(this->_v + i, *first);
 			}
 		}
 
+		// *************** Element access *************** //
+
+		reference at( size_type pos ) {
+			if (pos >= _sz) {
+				throw std::out_of_range("vector");
+			}
+			return _v[pos];
+		}
+		const_reference	at( size_type pos ) const {
+			if (pos >= _sz) {
+				throw std::out_of_range("vector");
+			}
+			return _v[pos];
+		}
+
+		reference		operator[]( size_type pos ) { return _v[pos]; }
+		const_reference	operator[]( size_type pos ) const { return _v[pos]; }
+		reference		front() { return *_v; }
+		const_reference	front() const { return *_v; }
+		reference		back() { return _v[_sz - 1]; }
+		const_reference	back() const { return _v[_sz - 1]; }
+
+		// *************** Element access *************** //
+
+
 		// *************** Capacity *************** //
+
 		bool		empty() const { return !_sz; }
 		size_t		size() const { return _sz; }
 		size_type	max_size() const { return std::numeric_limits< long long >::max() / sizeof( T ); }
@@ -97,23 +119,63 @@ namespace ft {
 				return;
 			}
 			T* newVector = _alloc.allocate(new_cap);
-//			std::uninitialized_copy(_pVector, _pVector + _sz, newVector); // TODO:: when be iter
+//			std::uninitialized_copy(_v, _v + _sz, newVector); // TODO:: when be iter
 			{
 				for (size_t i = 0; i < _sz; ++i) {
-					_alloc.construct(newVector + i, _pVector[i]);
+					_alloc.construct(newVector + i, _v[i]);
 				}
 			}
 			for (size_t i = 0; i < _sz; ++i) {
-				_alloc.destroy(_pVector + i);
+				_alloc.destroy(_v + i);
 			}
 			if (_cp) {
-				_alloc.deallocate(_pVector, _cp);
+				_alloc.deallocate(_v, _cp);
 			}
-			_pVector = newVector;
+			_v = newVector;
 			_cp = new_cap;
 		}
 		size_type	capacity() const { return _cp; }
 
+		// *************** Capacity *************** //
+
+
+		// *************** Modifiers *************** //
+
+		iterator erase( iterator pos ) {
+			for (size_type i = 0; i < _sz; ++i) {
+				if (&_v[i] == &(*pos)) {
+					_alloc.destroy(_v + i);
+					size_type el = i;
+					for (; i < _sz - 1; ++i) {
+						_alloc.construct(_v + i, _v[i + 1]);
+					}
+					if (el != i) {
+						_alloc.destroy(_v + i);
+					}
+					_sz -= 1;
+					return begin() + el;
+				}
+			}
+			return pos;
+
+		}
+
+		iterator erase( iterator first, iterator last ) {
+			for (size_type i = 0; i < _sz; ++i) {
+				if (&_v[i] == &(*first)) {
+					size_type el = i;
+					for (; first != last; ++i, ++first) {
+						_alloc.destroy(_v + i);
+					}
+					_sz -= i - el;
+					for (; el < _sz; ++i, ++el) {
+						_alloc.construct(_v + el, _v[i]);
+					}
+					return begin() + el;
+				}
+			}
+			return last;
+		}
 		void resize( size_t count, T value = T() ) {
 			if (count > _cp) {
 				size_t possible_size = _cp;
@@ -125,33 +187,25 @@ namespace ft {
 				reserve(count > possible_size ? count : possible_size);
 
 				for (size_t i = _sz; i < count; ++i) {
-					_alloc.construct(_pVector + i, value);
+					_alloc.construct(_v + i, value);
 				}
 			} else {
 				if (count < _sz) {
 					for (size_t i = count - 1; i < _sz; ++i) {
-						_alloc.destroy(_pVector + i);
+						_alloc.destroy(_v + i);
 					}
 				} else if (count != _sz) {
 					for (size_t i = _sz; i < count; ++i) {
-						_alloc.construct(_pVector + i, value);
+						_alloc.construct(_v + i, value);
 					}
 				}
 			}
 			_sz = count;
 		}
 
-		T& operator[](size_t n) {
-			return _pVector[n];
-		}
-
-		const T& operator[](size_t n) const {
-			return _pVector[n];
-		}
-
 
 	private:
-		value_type *_pVector;
+		value_type *_v;
 		allocator_type _alloc;
 		size_type _sz;
 		size_t _cp;
@@ -160,7 +214,7 @@ namespace ft {
 			reserve(count);
 			_sz = count;
 			for (size_t i = 0; i < count; ++i) {
-				_alloc.construct(_pVector + i, value);
+				_alloc.construct(_v + i, value);
 			}
 		}
 	};
