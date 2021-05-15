4a2f44770534d6728f9851317dcb54220269e4fd
MakhmudEgo <makhmudego@yandex.ru>
2021-05-15 17:00:00 +0300

    add revere iterator for vector

--- Iterator/Iterator.h
+++ Iterator/Iterator.h
@@ -105,3 +105,93 @@ public:
 	const_reference operator[]( int n ) const { return this->_i[ n ]; }
 
 };
+
+template<class T>
+class ReverseRandomAccess : public Iterator<T> {
+public:
+	typedef T				value_type;
+	typedef std::ptrdiff_t	difference_type;
+	typedef T*				pointer;
+	typedef T&				reference;
+	typedef const T&		const_reference;
+	typedef ReverseRandomAccess	iterator_type;
+
+
+	ReverseRandomAccess( T* i ) : Iterator< T >( i ) {}
+	ReverseRandomAccess(const Iterator<T>& i) : Iterator<T>(i) {}
+	ReverseRandomAccess( std::__1::__wrap_iter< T* > i) : Iterator< T >( i ) {}
+
+	reference operator*() { return *this->_i; }
+
+	// ***** post inc ***** //
+	iterator_type operator++( int ) {
+		return iterator_type( this->_i-- );
+	}
+
+	// ***** pre inc ***** //
+	iterator_type& operator++() {
+		--this->_i;
+		return *this;
+	}
+
+	// ***** post inc ***** //
+	iterator_type operator--( int ) {
+		return iterator_type( this->_i++ );
+	}
+
+	// ***** post dec ***** //
+	iterator_type& operator--() {
+		++this->_i;
+		return *this;
+	}
+
+	// ***** [] ***** //
+	reference operator[]( int n ) { return this->_i[ -n ]; }
+
+
+	// ***** + && - ***** //
+	iterator_type operator+( int n ) { return iterator_type(this->_i - n); }
+	iterator_type operator-( int n ) { return iterator_type(this->_i + n); }
+
+	difference_type operator+( const iterator_type& other ) { return this->_i - other._i; }
+	difference_type operator-( const iterator_type& other ) { return this->_i + other._i; }
+
+	// ***** += && -= ***** //
+	iterator_type operator+=( int n ) { return this->_i -= n; }
+	iterator_type operator-=( int n ) { return this->_i += n; }
+
+	// ***** > && < ***** //
+	bool operator>( const iterator_type& other ) { return this->_i < other._i; }
+	bool operator<( const iterator_type& other ) { return this->_i > other._i; }
+
+	// ***** >= && <= ***** //
+	bool operator>=( const iterator_type& other ) { return this->_i <= other._i; }
+	bool operator<=( const iterator_type& other ) { return this->_i >= other._i; }
+
+	// ***** == && != ***** //
+	bool operator==( const ReverseRandomAccess<T>& other ) {
+		return this->_i == other._i;
+	}
+	bool operator!=( const iterator_type& other ) { return this->_i != other._i; }
+
+	// ***** utils ***** //
+	iterator_type base() { return *this; }
+};
+
+template<class T>
+class ConstReverseRandomAccess : public ReverseRandomAccess<T> {
+public:
+	typedef T				value_type;
+	typedef std::ptrdiff_t	difference_type;
+	typedef T*				pointer;
+	typedef T&				reference;
+	typedef const T&		const_reference;
+
+	ConstReverseRandomAccess( T* i ) : ReverseRandomAccess< T >( i ) {}
+	ConstReverseRandomAccess( std::__1::__wrap_iter< T* > i) : ReverseRandomAccess< T >( i ) {}
+	ConstReverseRandomAccess(const Iterator<T>& i) : ReverseRandomAccess< T >( i ) {}
+
+	const_reference operator*() { return *this->_i; }
+	const_reference operator[]( int n ) const { return this->_i[ n ]; }
+
+};
