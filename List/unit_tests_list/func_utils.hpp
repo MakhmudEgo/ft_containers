461ad2fd94c11bd765fbb0b4e76a1387d36f058e
MakhmudEgo <makhmudego@yandex.ru>
2021-05-29 01:41:41 +0300

    add func utils for unit tests

---
+++ List/unit_tests_list/func_utils.hpp
@@ -0,0 +1,40 @@
+//
+// Created by Mahmud Jego on 5/28/21.
+//
+
+#pragma once
+
+template<typename T>
+std::ostream& PrintL(T& l, std::ostream& os) {
+	typename T::iterator it = l.begin();
+	for (size_t i = 0; it != l.end() ; ++it, ++i) {
+		if (i) {
+			os << "; ";
+		}
+		os << "l[" << i << "] = " << *it;
+	}
+	os << std::endl;
+	return os;
+}
+
+#define PRINT_L(x, y) \
+	PrintL(x, slRes); \
+	PrintL(y, mlRes);
+
+
+
+template<typename T, typename U>
+void InitList(T& l, U value, const bool dyn = true) {
+	typename T::iterator it = l.begin();
+	for (; it != l.end(); ++it) {
+		*it = dyn ? value++ : value;
+	}
+}
+
+#define INIT_LIST(x, y, z) \
+	InitList(x, z); \
+	InitList(y, z);
+
+#define SIZE_LIST \
+	slRes << "size " << sl.size() << std::endl; \
+	mlRes << "size " << ml.size() << std::endl;
