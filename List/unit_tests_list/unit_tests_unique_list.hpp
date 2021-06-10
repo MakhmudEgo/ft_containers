42176a0e111c354f6bbbc11afd0fb888fd523e95
MakhmudEgo <makhmudego@yandex.ru>
2021-06-10 13:23:22 +0500

    add unit tests for list.unique()

---
+++ List/unit_tests_list/unit_tests_unique_list.hpp
@@ -0,0 +1,65 @@
+//
+// Created by Mahmud Jego on 5/12/21.
+//
+
+#pragma once
+
+#include <list>
+#include "List.h"
+#include "sstream"
+#include "test_runner.h"
+#include "func_utils.hpp"
+
+
+
+void unit_tests_unique_list() {
+	{
+		std::list<int> sl(10);
+		ft::list<int> ml(10);
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique();
+		ml.unique();
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl(10);
+		ft::list<int> ml(10);
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		sl.push_back(19);
+		sl.push_back(19);
+		ml.push_back(19);
+		ml.push_back(19);
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique();
+		ml.unique();
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+
+}
