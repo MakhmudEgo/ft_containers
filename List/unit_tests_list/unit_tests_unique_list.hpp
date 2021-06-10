0ae105929607aaa0e6b815a4ea698e307bb05395
MakhmudEgo <makhmudego@yandex.ru>
2021-06-10 13:30:01 +0500

    add unit tests for list.unique() and list.unique(p);

--- List/unit_tests_list/unit_tests_unique_list.hpp
+++ List/unit_tests_list/unit_tests_unique_list.hpp
@@ -10,7 +10,7 @@
 #include "test_runner.h"
 #include "func_utils.hpp"
 
-
+static bool comp_unique(int a, int b) { return (a == b); }
 
 void unit_tests_unique_list() {
 	{
@@ -61,5 +61,150 @@ void unit_tests_unique_list() {
 		ASSERT_EQUAL(slRes.str(), mlRes.str());
 	}
 
+	{
+		std::list<int> sl(10);
+		ft::list<int> ml(10);
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique(comp_unique);
+		ml.unique(comp_unique);
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl(10);
+		ft::list<int> ml(10);
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		sl.push_back(19);
+		sl.push_back(19);
+		ml.push_back(19);
+		ml.push_back(19);
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique(comp_unique);
+		ml.unique(comp_unique);
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl;
+		ft::list<int> ml;
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		sl.push_back(19);
+		sl.push_back(19);
+		ml.push_back(19);
+		ml.push_back(19);
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique(comp_unique);
+		ml.unique(comp_unique);
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl;
+		ft::list<int> ml;
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+		sl.push_back(19);
+		sl.push_back(19);
+		ml.push_back(19);
+		ml.push_back(19);
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique();
+		ml.unique();
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl;
+		ft::list<int> ml;
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique();
+		ml.unique();
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
+
+	{
+		std::list<int> sl;
+		ft::list<int> ml;
+		std::stringstream slRes;
+		std::stringstream mlRes;
+
+		INIT_LIST(sl, ml, 10)
+
+		slRes << "before: ";
+		mlRes << "before: ";
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		slRes << "after: ";
+		mlRes << "after: ";
+		sl.unique(comp_unique);
+		ml.unique(comp_unique);
+		SIZE_LIST(sl, ml)
+		PRINT_L(sl, ml)
+
+		ASSERT_EQUAL(slRes.str(), mlRes.str());
+	}
 
 }
