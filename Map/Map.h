2c0091e71f76f6f6aa9de266028e51fb64025f3f
MakhmudEgo <makhmudego@yandex.ru>
2021-06-11 00:29:42 +0500

    start Map

---
+++ Map/Map.h
@@ -0,0 +1,16 @@
+//
+// Created by Makhmud Ego on 10.06.2021.
+//
+
+#pragma once
+
+namespace ft {
+	template<
+			class Key,
+			class T,
+			class Compare = std::less<Key>,
+			class Allocator = std::allocator<std::pair<const Key, T> >
+	> class map {
+
+	};
+}
---
+++ Map/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Makhmud Ego on 10.06.2021.
+//
+
+#include <iostream>
+#include "Map.h"
+
+int main() {
+	std::cout << "hello map:(" << std::endl;
+}
